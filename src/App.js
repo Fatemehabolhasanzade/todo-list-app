4453131224c7460398aa9f6329f7778f585c59b7
fatemeh <fatemeh.abolhasanzade@gmail.com>
2022-01-04 16:55:56 +0330

    dont forget your tasks!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { TextField, Button } from "@mui/material";
+import firebase from "firebase/app";
+// import 'firebase/firestore'
+import { todosColl } from './firebase-config'
+import TasksList from "./TasksList";
+import { color } from "@mui/system";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [taskList, setTaskList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false)
+
+
+
+
+  useEffect(() => {
+    getTasks();
+    setIsLoading(true);
+  }, [])
+
+  const addTask = async (e) => {
+    e.preventDefault();
+    console.log("you are trying to add a task");
+    todosColl.add({
+      todo: task,
+      inprogress: true,
+      time: firebase.firestore.FieldValue.serverTimestamp(),
+    })
+    setTask("");
+  }
+  const sortTasks = (arr) => {
+    return (
+      arr.sort()
+    )
+  }
+
+  const getTasks = () => {
+    todosColl.onSnapshot((querySnapshot) => {
+      setTaskList(sortTasks(querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        todo: doc.data().todo,
+        inprogress: doc.data().inprogress,
+        time: doc.data().time,
+      })))
+
+      );
+      setIsLoading(false);
+    });
+  }
+  console.log(taskList);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='container'>
+        <header><h1>todolist application</h1></header>
+        <form>
+          <TextField
+            id="outlined-basic"
+            className="task-input"
+            label="write a task"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
+            variant="outlined" />
+          <button
+            type="submit"
+            onClick={addTask}
+            style={{ display: "none" }}
+            variant="outlined">Outlined</button>
+
+        </form>
+
+        {/* show tasks */}
+        <div className="tasks">
+          {isLoading && <div className="loading">
+            data is loadind
+            <img src="Flying hearts.gif" alt="" />
+          </div>}
+          {!isLoading && taskList.map(item => {
+            return (
+              <TasksList key={item.id} todo={item.todo} inprogress={item.inprogress} id={item.id} />
+            )
+          })
+          }
+        </div>
+      </div>
+
     </div>
   );
 }
+
 
 export default App;
