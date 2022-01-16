429593fe20a088342febb82d9e89a2ed94f10001
fatemeh <fatemeh.abolhasanzade@gmail.com>
2022-01-16 10:21:46 +0330

    features added to project: *first page and last page *confirm delete *prevent empty tasks *is loading *contact us *paper note apearence problems: -states does not ture to false value!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState, useEffect } from "react";
-import { TextField, Button } from "@mui/material";
+// import { TextField, Button } from "@mui/material";
 import { supabase } from './client'
 import TasksList from "./TasksList";
 
@@ -9,9 +9,14 @@ function App() {
   const [taskList, setTaskList] = useState([]);
   const [isLoading, setIsLoading] = useState(false)
   const [isChanged, setIsChanged] = useState(false)
+  const [firstLoad, setFirstLoad] = useState(false)
+  const [closing, setClosing] = useState(false)
+  const [setting, setSetting] = useState(false)
+  const [noTodo, setNoTodo] = useState(false);
 
 
   useEffect(() => {
+    setFirstLoad(true);
     getTasks();
     setIsLoading(true);
   }, [])
@@ -20,106 +25,156 @@ function App() {
 
     const { data } = await supabase
       .from("todos")
-      .select()
+      .select('*')
     console.log(data);
     setTaskList(data.map(doc => ({
       id: doc.id,
       todo: doc.todo,
       inprogress: doc.inprogress,
-    })).sort((a, b) => a.id - b.id));
+    })).sort((a, b) => b.id - a.id));
     setIsLoading(false);
+
   }
+  const onChangeHandler = (e) => {
+    setTask(e.target.value);
+    setNoTodo(false);
+  }
+
 
   const addTask = async (e) => {
     e.preventDefault();
     console.log("you are trying to add a task");
-    await supabase
-      .from('todos')
-      .insert(
-        [
-          {
-            todo: task,
-            inprogress: true,
-          }
-        ]
-      )
-    setTask("");
+    if (task) {
+      setIsLoading(true);
+      await supabase
+        .from('todos')
+        .insert(
+          [
+            {
+              todo: task,
+              inprogress: true,
+            }
+          ]
+        )
+      setTask("");
+      getTasks();
+    } else {
+      setNoTodo(true);
+    }
+
   }
-  const mySubscription = supabase
-    .from('todos')
-    .on('INSERT', (payload) => {
-      getTasks()
-      console.log('Change received!', payload)
-    })
-    .on('DELETE', (payload) => {
-      getTasks()
-      console.log('Change received!', payload)
-    })
-    .on('UPDATE', (payload) => {
-      getTasks()
-      console.log('Change received!', payload)
-    })
-    .subscribe()
+  const startApp = () => {
+    setFirstLoad(false);
+  }
+  const exitApp = () => {
+    setSetting(false);
+    setClosing(true);
+  }
 
-
-
-  // const resetPage = () => {
-  //   setIsLoading(true)
-  //   setIsChanged(false)
-  //   window.location.reload()
-  // }
 
   return (
     <div className="App">
-      <div className='container'>
-        <header><h1>todolist application</h1></header>
-        <form>
-          <TextField
-            id="outlined-basic"
-            className="task-input"
-            label="write a task"
-            value={task}
-            onChange={(e) => setTask(e.target.value)}
-            variant="outlined" />
-          <Button
-            type="submit"
-            onClick={addTask}
-            style={{ display: "none" }}
-            variant="outlined">Outlined</Button>
+      {firstLoad && <div className="firstLoad">
+        <div className="wellcome">
+          <h1>welcom to the
+            <span> To Do List </span> application</h1>
 
-        </form>
 
-        {/* show tasks */}
+          <p className="owner">presented by <span>Rezvan</span></p>
+        </div>
+
+        <div className="startApp" onClick={startApp}>
+          <span>go to the app</span></div>
+      </div>}
+      {!firstLoad && !closing && <div className='container'>
+
         <div className="tasks">
-          {isLoading && <div className="loading">
-            data is loadind
-            <img src="Flying hearts.gif" alt="" />
-          </div>}
-          {!isLoading && taskList.map(item => {
-            return (
+          <div className="header">
+            <h1>To Do list</h1>
+            {!setting && <svg xmlns="http://www.w3.org/2000/svg"
+              width="16" height="16" fill="currentColor"
+              className="setting bi bi-gear-fill"
+              viewBox="0 0 16 16"
+              onClick={() => setSetting(true)}
+            >
+              <path d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 1 0-5.86 2.929 2.929 0 0 1 0 5.858z" />
+            </svg>}
+            {setting && <div className="settingItems">
+              <div className="login">
+                login
+              </div>
+              <div className="back" onClick={() => setSetting(false)}>
+                back
+              </div>
+              <div className="exitApp" onClick={exitApp}>
+                exit app
+              </div>
 
+            </div>}
+          </div>
+          {taskList.map(item => {
+            return (
               <TasksList
                 key={item.id}
                 todo={item.todo}
                 inprogress={item.inprogress}
                 id={item.id}
-                getTasks={() => getTasks}
+                getTasks={getTasks}
                 isChanged={isChanged}
                 setIsChanged={setIsChanged}
-
-
+                setIsLoading={setIsLoading}
               />
+
             )
           })
           }
+
+          <form className="form" onSubmit={addTask}>
+            < span >
+              what do you whant to do?
+            </span>
+            {!noTodo && <textarea
+              id="outlined-basic"
+              className="task-input"
+              label="write a task"
+              value={task}
+              onChange={onChangeHandler}
+              variant="outlined"
+            />}
+            {noTodo && <div className="noTodo">
+              please insert a task in the field first
+            </div>}
+
+            <button
+              type="submit"
+              onClick={() => setNoTodo(false)}
+              // style={{ display: "none" }}
+              variant="outlined">
+              {noTodo && "I get it!"}
+              {!noTodo && "write it!"}</button>
+          </form>
+          {isLoading && <div
+            className="loading">
+            please wait...
+          </div>}
         </div>
-        <Button style={{ display: (isLoading ? "block" : "none") }} >
-          something changes...please wait to update
-        </Button>
       </div>
+      }
+      {
+        closing &&
+        <div className="exitPage">
+          <div className="lastpage container">
+            <p className="thanks">Thank you!
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="red" className="bi bi-heart-fill" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z" />
+              </svg></p>
 
-
-    </div>
+            <a href="mailto:fatemeh.abolhasanzade@gmail.com" target={"_blank"}>contact us</a>
+            <p className="backToApp" onClick={() => setClosing(false)} >back to application</p>
+          </div>
+        </div>
+      }
+    </div >
   );
 }
 
--- src/TasksList.js
+++ src/TasksList.js
@@ -1,62 +1,107 @@
-import { Button, ListItem, ListItemText } from '@mui/material'
-import React from 'react'
+import {
+    Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle,
+} from '@mui/material'
+import React, { useState } from 'react'
 import { supabase } from "./client";
+
 
 const TasksList = ({
     todo,
     inprogress,
     id,
     getTasks,
-    isChanged,
-    setIsChanged
+    setIsLoading,
+
 }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+    const closeDialog = () => {
+        setOpen(false);
+        getTasks();
+    };
+
 
     const toggleProgress = async () => {
-        const { data } = await supabase
+        setIsLoading(true);
+        await supabase
             .from('todos')
             .update({ inprogress: !inprogress })
             .match({ id })
-        // getTasks();
-        setIsChanged(true);
-        console.log("task progress changed is :", data.todo);
+        getTasks();
 
     }
+
     const removeTask = async () => {
+        setOpen(false);
+        setIsLoading(true);
+
         const { data } = await supabase
             .from('todos')
             .delete()
             .match({ id })
-        // getTasks();
-        setIsChanged(true);
+        getTasks();
         console.log("task deleted is:", data.todo);
-
     }
-
-
-
 
 
     return (
         <div key={id} className='taskItem'>
-            <ListItem >
-                <ListItemText
-                    // className={` ${isChanged && "changed"}`}
-                    primary={todo}
-                    secondary={inprogress ? "In Progress" : "Completed"} />
+            <div className='listItem'>
+                <div className='taskEdit'>
+                    <div className='taskRemove' onClick={handleClickOpen} >
 
-            </ListItem>
-            <Button onClick={toggleProgress}            >
-                {inprogress ? "Done" : "unDone"}
-            </Button>
-            <Button onClick={removeTask}>
-                <svg xmlns="http://www.w3.org/2000/svg"
-                    width="16" height="16"
-                    fill="currentColor"
-                    className="bi bi-x-square"
-                    viewBox="0 0 16 16">
-                    <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
-                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-                </svg></Button>
+                        <svg xmlns="http://www.w3.org/2000/svg"
+                            width="16" height="16" fill="red"
+                            className="bi bi-x-square"
+                            viewBox="0 0 16 16">
+                            <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                        </svg>
+                        <div>
+                            <Dialog
+                                open={open}
+                                aria-labelledby="alert-dialog-title"
+                                aria-describedby="alert-dialog-description"
+                            >
+                                <DialogTitle id="alert-dialog-title">
+                                    {`are you sure you want to delete "${todo}"?`}
+                                </DialogTitle>
+                                <DialogContent>
+                                    <DialogContentText id="alert-dialog-description">
+                                        deleting the task will remove it from history,
+                                        are you still sure you want to delete it?
+                                    </DialogContentText>
+                                </DialogContent>
+                                <DialogActions>
+                                    <Button onClick={closeDialog} autoFocus> Forget about it!</Button>
+                                    <Button onClick={removeTask} > I'm sure.</Button>
+                                </DialogActions>
+                            </Dialog>
+                        </div>
+                    </div>
+                    <div onClick={toggleProgress} className="taskProgress" >
+                        {inprogress
+                            ? <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" className="bi bi-square" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                            </svg>
+                            : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="green" className="bi bi-check-square" viewBox="0 0 16 16">
+                                <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+                                <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z" />
+                            </svg>}
+                    </div>
+                </div>
+                <div className={`listItemText ${!inprogress ? "done" : undefined}`}>
+
+                    {todo}
+                </div>
+
+
+            </div>
+
+
         </div>
     )
 }
